516e678a300d2855df747d827031876f870e4b6a
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-19 18:50:06 -0500

    simple server running

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free Memory Info
+console.log(os.freemem());
+
+// Total Memory Info
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
