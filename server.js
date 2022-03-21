c2099ed2a35f86220c408555dfc6e1e3422b4c5b
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-21 11:37:39 -0500

    added skeleton routing

--- server.js
+++ server.js
@@ -2,17 +2,40 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    console.log(req.url);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch(req.url) {
+        case '/': 
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        //this is a reroute
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
     // send an html file 
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
+            res.end();
         } else {
-            res.write(data);
+            // res.write(data);
             res.end();
         }
     })
