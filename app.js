15bf33b9a5e1f017cf32e2abc84841e5f9c7b1cf
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-21 15:09:54 -0500

    paused to add new IP address

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
 //connect to mongodb
-const dbURI = 'mongodb+srv://simone:test1234@node-fun.pmw6z.mongodb.net/node-fun?retryWrites=true&w=majority';
+const dbURI = "mongodb+srv://simone:test1234@node-fun.pmw6z.mongodb.net/node-fun?retryWrites=true&w=majority";
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => console.log('connected to db'))
+    .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs')
 
 
-// middleware and static files
+// middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 app.get('/', (req, res) => {
   const blogs = [
