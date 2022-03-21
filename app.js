be76de512b0169c1ee76696a6baa5221ff4f2003
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-21 12:50:12 -0500

    adding ejs file views

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ const express = require('express');
 // express app
 const app = express();
 
+//register view enginer
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render();
 })
 
 //404 page creation, sequence important in express 
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404')
 });
