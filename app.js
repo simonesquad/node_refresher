e9613dce45a21c0236ea82311e17743565dc6f88
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-21 12:16:39 -0500

    reproduced routing in express

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    // res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
+});
 
-    res.send('<p>home page</p>');
+app.get('/about', (req, res) => {
+    // res.send('<p>about page</p>');
+    res.sendFile('./views/about.html', { root: __dirname });
+});
 
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+//404 page creation, sequence important in express 
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname })
 });
