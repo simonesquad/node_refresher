e8b96f0ff64b981febe0a44b3d702e0a526d29dd
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-21 11:15:02 -0500

    index html boilerplate added

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheets" href="#"></head>');
+    res.write('<p>hello, ninjas</p>');
+    res.write('<p>hello again, ninjas</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000')
-})
+});
